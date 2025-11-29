a2fda8d435111de783c818c451954a6fd9d12416
wyypoke <wyyhuayuan0813@163.com>
2025-11-29 18:23:17 +0800

    实现保存套餐

--- sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
@@ -1,9 +1,11 @@
 package com.sky.mapper;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.sky.entity.SetmealDish;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 @Mapper
-public interface SetmealDishMapper {
+public interface SetmealDishMapper extends BaseMapper<SetmealDish> {
     @Select("select count(id) from setmeal_dish where dish_id = #{dishId}")
     Integer countByDishId(Long dishId);
 }
